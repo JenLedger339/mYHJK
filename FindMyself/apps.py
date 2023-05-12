33aba0fd725f0b0b04bed74e57bbc9740d3438f6
Ibragim <132456mic@gmail.com>
2023-05-12 12:44:11 +0300

    Add a connection with user and profile Now creating a profile leads to creating a profile

--- FindMyself/apps.py
+++ FindMyself/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class FindmyselfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'FindMyself'
+    def ready(self):
+        import FindMyself.signals
